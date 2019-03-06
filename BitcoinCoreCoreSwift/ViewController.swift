d9ba72ed5826aa38c164a9b942b7e1449742eda2
jackieyip <jiajie.ye@rylink.com>
2019-03-06 11:14:14 +0800

    Initial Commit

---
+++ BitcoinCoreCoreSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinCoreCoreSwift
+//
+//  Created by jackieyip on 2019/3/6.
+//  Copyright © 2019 jackieyip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
